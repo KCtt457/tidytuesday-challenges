93d81f01f3be82730efd2bd649d982dfa195fcf7
KCtt457 <chinkam123@gmail.com>
2021-04-12 21:36:58 -0400

    test

---
+++ Week13/week13.R
@@ -0,0 +1,6 @@
+# TidyTuesday Week 13: UN Votes
+
+# Get the data
+unvotes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/unvotes.csv')
+roll_calls <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/roll_calls.csv')
+issues <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/issues.csv')
